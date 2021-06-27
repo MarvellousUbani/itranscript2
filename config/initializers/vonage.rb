e005baac67834cba8ff2f61bc61cacf0ed066154
juxsalley <juxsalley@gmail.com>
2021-06-27 21:20:39 +0000

    update

--- config/initializers/vonage.rb
+++ config/initializers/vonage.rb
@@ -1,4 +1,12 @@
-Vonage.setup do |config|
-  config.api_key = Rails.application.credentials.nexmo.key
-  config.api_secret = Rails.application.credentials.nexmo.secret
+if Rails.env.production?
+  Vonage.setup do |config|
+    config.api_key = ENV['nex-key']
+    config.api_secret = ENV['nex-secret']
+  end
+else
+
+  Vonage.setup do |config|
+    config.api_key = Rails.application.credentials.nexmo.key
+    config.api_secret = Rails.application.credentials.nexmo.secret
+  end
 end
